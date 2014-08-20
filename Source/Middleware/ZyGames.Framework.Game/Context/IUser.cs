e472f6093bcdd268fa6ade5c69782eea90b720b2
ScutGame <wzf_88@qq.com>
2014-08-20 10:30:21 +0800

    release lib 6.7.8.7

---
+++ Source/Middleware/ZyGames.Framework.Game/Context/IUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZyGames.Framework.Game.Context
+{
+    /// <summary>
+    /// IUser
+    /// </summary>
+    public interface IUser
+    {
+        /// <summary>
+        /// user's token
+        /// </summary>
+        string Token { get; set; }
+        /// <summary>
+        /// is lock status
+        /// </summary>
+        bool IsLock { get; }
+
+ 
+        /// <summary>
+        /// is online
+        /// </summary>
+        bool IsOnlining { get; }
+
+        /// <summary>
+        /// get userid
+        /// </summary>
+        /// <returns></returns>
+       int GetUserId();
+        /// <summary>
+        /// get passport
+        /// </summary>
+        /// <returns></returns>
+        string GetPassportId();
+        /// <summary>
+        /// reflesh date
+        /// </summary>
+        void RefleshOnlineDate();
+
+    }
+}
